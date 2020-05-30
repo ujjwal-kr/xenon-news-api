aca6c6fe81929ff89677c9aa342a2d528df47451
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-05-30 20:55:11 +0530

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const port = 3000 || process.env.PORT
+app.get('/', async (req, res) => {
+    await axios.get("https://news.google.com/rss")
+    .then(data => {
+        res.send(data.data)
+    })
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port port!');
+});
+
+//Run app, then load http://localhost:port in a browser to see the output.
