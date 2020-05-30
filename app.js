10d9ef673d3c19cce6d6124c9616ad905e52ae3a
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-05-30 22:21:14 +0530

    init

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express();
 const axios = require('axios');
 const port = 3000 || process.env.PORT
 app.get('/', async (req, res) => {
-    await axios.get("https://news.google.com/rss/search?q=technology", {
-        headers: {"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"},
-    })
-    .then(data => {
-        res.send(data.data)
-    }).catch(e => {
-        res.send(e)
+    const arr = ['4', '4', '4','4', '4', '4','4', '4', '4','4', '4', '4','4', '4', '4','4', '4', '4','4', '4', '4','4', '4', '4','4', '4', '4']
+    arr.forEach(event => {
+        await axios.get("https://news.google.com/rss/search?q=technology", {
+            headers: {"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"},
+        })
+        .then(data => {
+            res.send(data.data)
+        }).catch(e => {
+            res.send(e)
+        })
     })
 });
 
