66880d5c818fc43181903e411c3acefb6c33111e
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-06-16 00:31:25 +0530

    CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+var cors = require('cors')
 var convert = require('xml-js');
 
-const port = 3000 || process.env.PORT
+const port = 3000 || process.env.PORT;
+app.use(cors())
 app.get('/', async (req, res) => {
     await axios.get("https://news.google.com/rss/search?q=technology", {
         headers: {"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"},
@@ -20,5 +22,3 @@ app.get('/', async (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log('Example app listening on port port!');
 });
-
-//Run app, then load http://localhost:port in a browser to see the output.
